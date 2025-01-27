489aef490b9dc2e5ad7c512da01a30aef238913e
Serhii Zamikhovskyi <s.zamikhovskyi@platform.live>
2025-01-27 17:20:34 +0100

    Fixed docker-compose.yaml, added bash scripts for building project.

--- src/main/java/org/testProject/controller/CucumberController.java
+++ src/main/java/org/testProject/controller/CucumberController.java
@@ -6,12 +6,14 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import lombok.extern.slf4j.Slf4j;
 import org.testProject.model.Cucumber;
 import org.testProject.service.CucumberService;
 
 import java.util.List;
 
 @Path("/cucumber")
+@Slf4j
 public class CucumberController {
 
     private CucumberService cucumberService;
@@ -24,6 +26,7 @@ public class CucumberController {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Cucumber> getCucumbers(@QueryParam("q") int quantity) {
+        log.info("Getting cucumbers..");
         return cucumberService.getCucumbers(quantity);
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,22 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+SELECT 'CREATE DATABASE cucumber'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cucumber');
+
+CREATE ROLE jonathan WITH CREATEDB CREATEROLE LOGIN SUPERUSER PASSWORD 'admin';
+GRANT ALL PRIVILEGES on DATABASE cucumber TO jonathan;
+ALTER USER jonathan PASSWORD 'admin';
+
+CREATE TABLE IF NOT EXISTS cucumbers (
+    uid varchar primary key,
+    csize int not null,
+    sizeCategory varchar not null,
+    pimpleCount int not null,
+    address  varchar
+);
+
+INSERT INTO cucumbers (uid, csize, sizeCategory, pimpleCount, address) VALUES ('daea0d59-308c-47c3-b5c0-3484ef3ee7cf', 4, 'MIDDLE', 62, 'Address 1');
+INSERT INTO cucumbers (uid, csize, sizeCategory, pimpleCount, address) VALUES ('9698458a-a184-4716-b2b6-466e7fdb581d', 4, 'SMALL', 72, 'Address 2');
+
